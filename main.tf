34ea7dd6f8438a97d7b4da4e74374f06b2fcd474
clay <hatimsue@gmail.com>
2023-07-06 19:38:03 -0500

    :ambulance: Delete s3_bucket_acl

--- main.tf
+++ main.tf
@@ -6,11 +6,6 @@ resource "aws_s3_bucket" "this" {
   bucket_prefix = var.bucket_prefix
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
